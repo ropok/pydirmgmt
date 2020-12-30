5e900725c667ff69a30dcf512aa109af14a3b227
Jaler Sekar Maji <jalerse@gmail.com>
2020-12-30 19:45:02 +0700

    feat(list-hp-audio): update try except for counting

--- listHandphoneAudio.py
+++ listHandphoneAudio.py
@@ -65,17 +65,24 @@ class Main():
     def wavCount(self, vt_path):
         # os.chdir(vt_name)
         # count_dict = {}
-        cnt = collections.Counter()
-        for filename in glob.glob(os.path.join(vt_path,"*.wav")):
-            try:
-                name, ext = os.path.splitext(filename)
-                cnt[ext] += 1
-            except:
-                print("no file exist")
-        # count_dict['username'] = vt_name
-        # count_dict['jumlah file'] = cnt[ext]
-        # print(cnt[ext])
-        return cnt[ext]
+        # cnt = collections.Counter()
+        # for filename in glob.glob(os.path.join(vt_path,"*.wav")):
+        #     try:
+        #         name, ext = os.path.splitext(filename)
+        #         cnt[ext] += 1
+        #     except:
+        #         print("no file exist")
+        # # count_dict['username'] = vt_name
+        # # count_dict['jumlah file'] = cnt[ext]
+        # # print(cnt[ext])
+        # return cnt[ext]
+        count_wav = 0
+        for root, dirs, files in os.walk(vt_path):
+            for file in files:
+                if file.endswith('.wav'):
+                    count_wav += 1
+        return count_wav
+
 
 if __name__ == '__main__':
     path = 'C:/Users/jalerse/Downloads/data_hp'
